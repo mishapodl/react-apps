c471d5a37b260ea430a9e427d0d38f33dfc7c1ce
mishapodl <mishapod@gmail.com>
2020-06-02 13:33:48 +0300

    Поменял структуру трелло

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { AppConteiner } from './styles';
-import { Card } from './Card';
-import { Column } from './Column';
-import { AddNewItem } from './AddNewItem';
-import { useAppState } from './AppStateContext';
+import Trello from './trello/Trello';
+import { AppStateProvider } from './trello/AppStateContext';
 
 const App = () => {
-    const { state } = useAppState();
     return (
-        <AppConteiner>
-            {state.lists.map((list, i) => (
-                <Column text={list.text} key={list.id} index={i} />
-            ))}
-            <AddNewItem
-                toggleButtonText="+ Add another list"
-                onAdd={console.log}
-            />
-        </AppConteiner>
+        <AppStateProvider>
+            <Trello />
+        </AppStateProvider>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { AppStateProvider } from './AppStateContext';
 
-ReactDOM.render(
-    <AppStateProvider>
-        <App />
-    </AppStateProvider>,
-    document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
