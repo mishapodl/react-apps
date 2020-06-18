65c3c605f37c13387f59c0b7ab4ca09509126383
mishapodl <mishapod@gmail.com>
2020-06-19 01:04:36 +0300

    Удалил трелло

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import PizzaShop from './pizza-shop/App';
-import Trello from './trello/Trello';
-import { AppStateProvider } from './trello/AppStateContext';
+// import Trello from './trello/Trello';
+// import { AppStateProvider } from './trello/AppStateContext';
 
 function App() {
     return (
         <>
             <PizzaShop />
-            <AppStateProvider>
-                <Trello />
-            </AppStateProvider>
         </>
     );
 }
